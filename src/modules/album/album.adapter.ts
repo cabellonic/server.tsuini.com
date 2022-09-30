f94774cb6ea7c0662445ed636b1d218ac9762a8a
Nicolás Cabello <cabellonic@gmail.com>
2022-09-30 18:58:51 -0300

    update: Add album adapter

---
+++ src/modules/album/album.adapter.ts
@@ -0,0 +1,14 @@
+import { NewAlbumEntry } from "../../models";
+
+export const adaptAlbumFromSpotify = (albumFromSpotify: any): NewAlbumEntry => {
+	return {
+		id: albumFromSpotify.id,
+		name: albumFromSpotify.name,
+		albumType: albumFromSpotify.album_type,
+		href: albumFromSpotify.href,
+		image: albumFromSpotify.images[0]?.url,
+		releaseDate: albumFromSpotify.release_date,
+		spotifyUrl: albumFromSpotify.external_urls.spotify,
+		totalSongs: albumFromSpotify.total_tracks,
+	};
+};
