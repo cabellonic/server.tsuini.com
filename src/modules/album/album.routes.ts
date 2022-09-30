689fb0a0163da697dee6c1a80cc0158fb3379e58
Nicolás Cabello <cabellonic@gmail.com>
2022-09-30 19:00:47 -0300

    feature: Add album routes

---
+++ src/modules/album/album.routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+// Services
+import * as albumController from "./album.controller";
+
+const router = express.Router();
+
+router.get("/", albumController.getAlbums);
+router.get("/:id", albumController.getAlbumByID);
+
+export default router;
