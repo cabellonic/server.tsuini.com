d96407b56f10fda4b6044854b244528318985358
Nicolás Cabello <cabellonic@gmail.com>
2022-09-29 23:42:43 -0300

    update: Implement axios utils

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,13 +1,11 @@
 import axios from "axios";
 // Adapter
 import * as authAdapter from "./auth.adapters";
+// Utils
+import * as utils from "../../utils";
 
 export const getUserFromSpotify = async (accessToken: string) => {
-	const config = {
-		headers: {
-			Authorization: `Bearer ${accessToken}`,
-		},
-	};
+	const config = utils.getAxiosConfig({ accessToken });
 
 	const { data } = await axios.get("https://api.spotify.com/v1/me", config);
 	return authAdapter.adaptUserFromSpotify(data);
