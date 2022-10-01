b84d02181f720c1584e33db8ae163948b2770deb
Nicolás Cabello <cabellonic@gmail.com>
2022-09-30 23:04:27 -0300

    update: Add artist adapter

---
+++ src/modules/artist/artist.adapter.ts
@@ -0,0 +1,10 @@
+import { NewArtistEntry } from "../../models";
+
+export const adaptArtistFromSpotify = (data: any): NewArtistEntry => {
+	return {
+		id: data.id,
+		name: data.name,
+		avatar: data.images[0]?.url,
+		spotifyUrl: data.external_urls.spotify,
+	};
+};
