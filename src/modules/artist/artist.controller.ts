cf67593690a2be354542ab0b25fa9b090eb14b60
Nicolás Cabello <cabellonic@gmail.com>
2022-09-30 23:05:05 -0300

    update: Add artist controller

---
+++ src/modules/artist/artist.controller.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from "express";
+// Service
+import * as artistService from "./artist.service";
+
+export const getArtists = async (_req: Request, res: Response, _next: NextFunction) => {
+	const artists = await artistService.getArtists();
+	return res.json(artists);
+};
+
+export const getArtistByID = async (req: Request, res: Response, _next: NextFunction) => {
+	const { id } = req.params;
+	const artist = await artistService.getArtistByID(id);
+	if (!artist) return res.status(404).end();
+	return res.json(artist);
+};
