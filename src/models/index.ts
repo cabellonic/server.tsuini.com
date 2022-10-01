798303eeb4ffe33e30837189d47c22c284afbad0
Nicolás Cabello <cabellonic@gmail.com>
2022-09-30 23:04:07 -0300

    update: Add artist model to model barrel and typeorm data source

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,3 +2,4 @@ export * from "../modules/session/session.model";
 export * from "../modules/user/user.model";
 export * from "../modules/song/song.model";
 export * from "../modules/album/album.model";
+export * from "../modules/artist/artist.model";
