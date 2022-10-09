5efa7231779909f0bc7fb5f647f3e85ef3cf72c3
Nicolás Cabello <cabellonic@gmail.com>
2022-10-09 20:25:38 -0300

    fix: Wrap axios request with try catch

--- src/modules/player/player.service.ts
+++ src/modules/player/player.service.ts
@@ -1,85 +1,163 @@
 import axios from "axios";
 // Models
 import { Devices, PlaybackState } from "./player.model";
+// Config
+import * as playerConfig from "./player.config";
 // Utils
 import * as utils from "../../utils";
 
-const SPOTIFY_API_URL = "https://api.spotify.com/v1/me";
+export const getPlaybackState = async (
+  accessToken: string
+): Promise<PlaybackState> => {
+  const config = utils.getAxiosConfig({ accessToken });
 
-export const getPlaybackState = async (accessToken: string): Promise<PlaybackState> => {
-	const config = utils.getAxiosConfig({ accessToken });
-
-	const { data } = await axios.get(`${SPOTIFY_API_URL}/player`, config);
-	return data;
+  try {
+    const { data } = await axios.get(`${playerConfig.PLAYER_API_URI}`, config);
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const setPlaybackDevice = async (accessToken: string, body: any): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+export const setPlaybackDevice = async (
+  accessToken: string,
+  body: any
+): Promise<void> => {
+  const config = utils.getAxiosConfig({ accessToken });
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player`, body, config);
-	return data;
+  await axios.put(`${playerConfig.PLAYER_API_URI}`, body, config);
 };
 
 export const setPlaybackPlay = async (accessToken: string): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+  const config = utils.getAxiosConfig({ accessToken });
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player/play`, null, config);
-	return data;
+  try {
+    await axios.put(
+      `${playerConfig.PLAYER_API_URI}/play`,
+      new URLSearchParams({}),
+      config
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const setPlaybackPause = async (accessToken: string): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+  const config = utils.getAxiosConfig({ accessToken });
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player/pause`, null, config);
-	return data;
+  try {
+    await axios.put(`${playerConfig.PLAYER_API_URI}/pause`, null, config);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const setPlaybackRepeat = async (accessToken: string, body: any): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+export const setPlaybackRepeat = async (
+  accessToken: string,
+  body: any
+): Promise<void> => {
+  const config = utils.getAxiosConfig({ accessToken });
+  const query = utils.getQueryString(body);
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player/repeat`, body, config);
-	return data;
+  try {
+    await axios.put(
+      `${playerConfig.PLAYER_API_URI}/repeat${query}`,
+      null,
+      config
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export const setPlaybackNext = async (accessToken: string): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+  const config = utils.getAxiosConfig({ accessToken });
 
-	const { data } = await axios.post(`${SPOTIFY_API_URL}/player/next`, null, config);
-	return data;
+  try {
+    await axios.post(`${playerConfig.PLAYER_API_URI}/next`, null, config);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const setPlaybackPrevious = async (accessToken: string): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+export const setPlaybackPrevious = async (
+  accessToken: string
+): Promise<void> => {
+  const config = utils.getAxiosConfig({ accessToken });
 
-	const { data } = await axios.post(`${SPOTIFY_API_URL}/player/previous`, null, config);
-	return data;
+  try {
+    await axios.post(`${playerConfig.PLAYER_API_URI}/previous`, null, config);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const setPlaybackSeek = async (accessToken: string, body: any): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
-	const query = utils.getQueryString(body);
+export const setPlaybackSeek = async (
+  accessToken: string,
+  body: any
+): Promise<void> => {
+  const config = utils.getAxiosConfig({ accessToken });
+  const query = utils.getQueryString(body);
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player/seek${query}`, null, config);
-	return data;
+  try {
+    await axios.put(
+      `${playerConfig.PLAYER_API_URI}/seek${query}`,
+      null,
+      config
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const setPlaybackShuffle = async (accessToken: string, body: any): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+export const setPlaybackShuffle = async (
+  accessToken: string,
+  body: any
+): Promise<void> => {
+  const config = utils.getAxiosConfig({ accessToken });
+  const query = utils.getQueryString(body);
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player/shuffle`, body, config);
-	return data;
+  try {
+    await axios.put(
+      `${playerConfig.PLAYER_API_URI}/shuffle${query}`,
+      null,
+      config
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const setPlaybackVolume = async (accessToken: string, body: any): Promise<void> => {
-	const config = utils.getAxiosConfig({ accessToken });
+export const setPlaybackVolume = async (
+  accessToken: string,
+  body: any
+): Promise<void> => {
+  const config = utils.getAxiosConfig({ accessToken });
+  const query = utils.getQueryString(body);
 
-	const { data } = await axios.put(`${SPOTIFY_API_URL}/player/volume`, body, config);
-	return data;
+  try {
+    await axios.put(
+      `${playerConfig.PLAYER_API_URI}/volume${query}`,
+      null,
+      config
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const getPlaybackDevices = async (accessToken: string): Promise<Devices> => {
-	const config = utils.getAxiosConfig({ accessToken });
+export const getPlaybackDevices = async (
+  accessToken: string
+): Promise<Devices> => {
+  const config = utils.getAxiosConfig({ accessToken });
 
-	const { data } = await axios.get(`${SPOTIFY_API_URL}/player/devices`, config);
-	return data;
+  try {
+    const { data } = await axios.get(
+      `${playerConfig.PLAYER_API_URI}/devices`,
+      config
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 };
