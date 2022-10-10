4cb1fcd68c630cac89a45ff1ec9012b44d32f844
Nicolás Cabello <cabellonic@gmail.com>
2022-10-10 18:22:58 -0300

    update: Add catch Async Errors wrapper to auth controllers

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 // Controllers
 import * as authController from './auth.controller';
+// Utils
+import { catchAsyncErrors } from '../../utils';
 
 const router = express.Router();
 
-router.get('/login', authController.login);
-router.get('/callback', authController.authCallback);
-router.get('/me', authController.getMe);
-router.get('/logout', authController.logout);
-router.get('/refresh', authController.refresh);
+router.get('/login', catchAsyncErrors(authController.login));
+router.get('/callback', catchAsyncErrors(authController.authCallback));
+router.get('/me', catchAsyncErrors(authController.getMe));
+router.get('/logout', catchAsyncErrors(authController.logout));
+router.get('/refresh', catchAsyncErrors(authController.refresh));
 
 export default router;
