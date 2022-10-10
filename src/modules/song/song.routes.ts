b600f34a143fef0c5716d784edea08b85937dc84
Nicolás Cabello <cabellonic@gmail.com>
2022-10-10 18:23:21 -0300

    update: Add catch Async Errors wrapper to song controllers

--- src/modules/song/song.routes.ts
+++ src/modules/song/song.routes.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 // Services
 import * as songService from './song.controller';
+// Utils
+import { catchAsyncErrors } from '../../utils';
 
 const router = express.Router();
 
-router.get('/', songService.getSongs);
-router.get('/:id', songService.getSongByID);
+router.get('/', catchAsyncErrors(songService.getSongs));
+router.get('/:id', catchAsyncErrors(songService.getSongByID));
 
 export default router;
