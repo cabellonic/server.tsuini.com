89b60500ceb605e743d525fa38471bf6799e48bf
Nicolás Cabello <cabellonic@gmail.com>
2022-10-10 18:23:30 -0300

    update: Add catch Async Errors wrapper to user controllers

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 // Controllers
 import * as userController from './user.controller';
+// Utils
+import { catchAsyncErrors } from '../../utils';
 
 const router = express.Router();
 
-router.get('/', userController.getUsers);
-router.get('/:id', userController.getUserByID);
-router.get('/filter', userController.getUsersByCriteria);
+router.get('/', catchAsyncErrors(userController.getUsers));
+router.get('/:id', catchAsyncErrors(userController.getUserByID));
+router.get('/filter', catchAsyncErrors(userController.getUsersByCriteria));
 
 export default router;
