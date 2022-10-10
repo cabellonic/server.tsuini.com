ecf819df9c91aa4ca542594aa81861d17be87f09
Nicolás Cabello <cabellonic@gmail.com>
2022-10-10 20:25:21 -0300

    update: Session stores user id instead user object

--- types/session.d.ts
+++ types/session.d.ts
@@ -4,6 +4,7 @@ import { User } from '../src/models';
 declare module 'express-session' {
 	interface SessionData {
 		user: User;
+		user_id: string;
 		tokens: {
 			access_token: string;
 			refresh_token: string;
