f01ee75310d80cc8d174893c5746d7ef5a68b184
Nicolás Cabello <cabellonic@gmail.com>
2022-10-10 18:21:27 -0300

    update: Add error handler middleware

---
+++ src/middlewares/error.middleware.ts
@@ -0,0 +1,9 @@
+import { AxiosError } from 'axios';
+import { ErrorRequestHandler } from 'express';
+
+export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
+	if (error instanceof AxiosError) {
+		return res.status(error.response.status).json(error.response.data);
+	}
+	return res.status(500).json({ message: 'Internal server error.' });
+};
