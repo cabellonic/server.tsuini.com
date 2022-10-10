0e551021c8f18bc4dd9b6703411b6aebaf63d609
Nicolás Cabello <cabellonic@gmail.com>
2022-10-10 18:23:12 -0300

    update: Add catch Async Errors wrapper to player controllers

--- src/modules/player/player.routes.ts
+++ src/modules/player/player.routes.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 // Controllers
 import * as playerController from './player.controller';
+// Utils
+import { catchAsyncErrors } from '../../utils';
 
 const router = express.Router();
 
 // Player controller
-router.get('/', playerController.getPlaybackState);
-router.put('/', playerController.setPlaybackDevice);
-router.put('/play', playerController.setPlaybackPlay);
-router.put('/pause', playerController.setPlaybackPause);
-router.put('/repeat', playerController.setPlaybackRepeat);
-router.post('/next', playerController.setPlaybackNext);
-router.post('/previous', playerController.setPlaybackPrevious);
-router.put('/seek', playerController.setPlaybackSeek);
-router.put('/shuffle', playerController.setPlaybackShuffle);
-router.put('/volume', playerController.setPlaybackVolume);
-router.get('/devices', playerController.getPlaybackDevices);
+router.get('/', catchAsyncErrors(playerController.getPlaybackState));
+router.put('/', catchAsyncErrors(playerController.setPlaybackDevice));
+router.put('/play', catchAsyncErrors(playerController.setPlaybackPlay));
+router.put('/pause', catchAsyncErrors(playerController.setPlaybackPause));
+router.put('/repeat', catchAsyncErrors(playerController.setPlaybackRepeat));
+router.post('/next', catchAsyncErrors(playerController.setPlaybackNext));
+router.post('/previous', catchAsyncErrors(playerController.setPlaybackPrevious));
+router.put('/seek', catchAsyncErrors(playerController.setPlaybackSeek));
+router.put('/shuffle', catchAsyncErrors(playerController.setPlaybackShuffle));
+router.put('/volume', catchAsyncErrors(playerController.setPlaybackVolume));
+router.get('/devices', catchAsyncErrors(playerController.getPlaybackDevices));
 
 // Playlist controller
 // router.get("/queue", () => {});
