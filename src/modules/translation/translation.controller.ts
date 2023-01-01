f4ae5caad6552b137d59338d457e3b882b053a09
Nicolás Cabello <cabellonic@gmail.com>
2022-12-31 21:28:38 -0300

    fix: Translation controller does not wrap service response with an object

--- src/modules/translation/translation.controller.ts
+++ src/modules/translation/translation.controller.ts
@@ -4,36 +4,36 @@ import * as translationService from './translation.service';
 
 export const getTranslations = async (req: Request, res: Response, _next: NextFunction) => {
 	const translations = await translationService.getTranslations();
-	return res.json({ translations });
+	return res.json(translations);
 };
 
 export const getTranslationByID = async (req: Request, res: Response, _next: NextFunction) => {
 	const { id } = req.params;
 	const translation = await translationService.getTranslationByID(id);
-	return res.json({ translation });
+	return res.json(translation);
 };
 
 export const getTranslationsByCriteria = async (req: Request, res: Response, _next: NextFunction) => {
 	const criteria = req.query;
 	const translations = await translationService.getTranslationsByCriteria(criteria);
-	return res.json({ translations });
+	return res.json(translations);
 };
 
 export const createTranslation = async (req: Request, res: Response, _next: NextFunction) => {
 	const translation = req.body;
 	const newTranslation = await translationService.createTranslation(translation);
-	return res.json({ newTranslation });
+	return res.json(newTranslation);
 };
 
 export const updateTranslation = async (req: Request, res: Response, _next: NextFunction) => {
 	const { id } = req.params;
 	const translation = req.body;
 	const updatedTranslation = await translationService.updateTranslation(id, translation);
-	return res.json({ updatedTranslation });
+	return res.json(updatedTranslation);
 };
 
 export const deleteTranslation = async (req: Request, res: Response, _next: NextFunction) => {
 	const { id } = req.params;
 	const deletedTranslation = await translationService.deleteTranslation(id);
-	return res.json({ deletedTranslation });
+	return res.json(deletedTranslation);
 };
