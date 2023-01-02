c41c438bfa92ed95680de0588bb8463f77a3ee46
Nicolás Cabello <cabellonic@gmail.com>
2023-01-02 19:17:40 -0300

    update: Add getArtistFromSpotify and createArtist to artist controller

--- src/modules/artist/artist.controller.ts
+++ src/modules/artist/artist.controller.ts
@@ -9,7 +9,28 @@ export const getArtists = async (_req: Request, res: Response, _next: NextFuncti
 
 export const getArtistByID = async (req: Request, res: Response, _next: NextFunction) => {
 	const { id } = req.params;
+	const artistFromDB = await artistService.getArtistByID(id);
+	if (artistFromDB) return res.json(artistFromDB);
+	const artistFromSpotify = await artistService.getArtistFromSpotify(req.session?.tokens?.access_token, id);
+	return res.json(artistFromSpotify);
+};
+
+export const getArtistFromDB = async (req: Request, res: Response, _next: NextFunction) => {
+	const { id } = req.params;
 	const artist = await artistService.getArtistByID(id);
 	if (!artist) return res.status(404).end();
 	return res.json(artist);
+};
+
+export const getArtistFromSpotify = async (req: Request, res: Response, _next: NextFunction) => {
+	const { id } = req.params;
+	const artist = await artistService.getArtistFromSpotify(req.session?.tokens?.access_token, id);
+	return res.json(artist);
+};
+
+export const createArtist = async (req: Request, res: Response, _next: NextFunction) => {
+	const alreadyExist = Boolean(await artistService.getArtistByID(req.body?.id));
+	if (alreadyExist) return res.status(409).json({ message: 'Entity Already Exists' });
+	const createdArtist = await artistService.createArtist(req.body);
+	return res.json(createdArtist);
 };
