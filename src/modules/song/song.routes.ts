c279eb6674ed12db07484841f0f2d3765cff7574
Nicolás Cabello <cabellonic@gmail.com>
2023-01-02 23:46:11 -0300

    feature: Add get song from spotify and create song routes

--- src/modules/song/song.routes.ts
+++ src/modules/song/song.routes.ts
@@ -8,5 +8,7 @@ const router = express.Router();
 
 router.get('/', catchAsyncErrors(songService.getSongs));
 router.get('/:id', catchAsyncErrors(songService.getSongByID));
+router.get('/spotify/:id', catchAsyncErrors(songService.getSongFromSpotify));
+router.post('/', catchAsyncErrors(songService.createSong));
 
 export default router;
