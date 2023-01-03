2905c59b87acb327d9496eb9a96d73d38bf03c41
Nicolás Cabello <cabellonic@gmail.com>
2023-01-02 22:19:16 -0300

    update: Album adapter now fits the Album model

--- src/modules/album/album.adapter.ts
+++ src/modules/album/album.adapter.ts
@@ -4,11 +4,14 @@ export const adaptAlbumFromSpotify = (albumFromSpotify: any): NewAlbumEntry => {
 	return {
 		id: albumFromSpotify.id,
 		name: albumFromSpotify.name,
+		completed: false,
 		albumType: albumFromSpotify.album_type,
 		href: albumFromSpotify.href,
 		image: albumFromSpotify.images[0]?.url,
 		releaseDate: albumFromSpotify.release_date,
 		spotifyUrl: albumFromSpotify.external_urls.spotify,
 		totalSongs: albumFromSpotify.total_tracks,
+		uploader: null,
+		artists: albumFromSpotify.artists,
 	};
 };
