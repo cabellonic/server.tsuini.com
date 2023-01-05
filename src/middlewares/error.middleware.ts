3c590f62064271a5ed8860537273e18cf2fc7961
Nicolás Cabello <cabellonic@gmail.com>
2023-01-04 22:11:47 -0300

    update: Error middleware now log the error message in console

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -2,6 +2,7 @@ import { AxiosError } from 'axios';
 import { ErrorRequestHandler } from 'express';
 
 export const errorHandler: ErrorRequestHandler = (error, _req, res, _next) => {
+	console.error(error.message);
 	if (error instanceof AxiosError) {
 		return res.status(error.response.status).json(error.response.data);
 	}
