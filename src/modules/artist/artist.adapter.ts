dd3d3e9661b638948dfb284aecdf1e6774e3e86a
Nicolás Cabello <cabellonic@gmail.com>
2023-01-11 00:23:49 -0300

    update: Use album adapter to format albums in artist adapter

--- src/modules/artist/artist.adapter.ts
+++ src/modules/artist/artist.adapter.ts
@@ -1,11 +1,13 @@
-import { NewArtistEntry } from '../../models';
+import { Album, NewArtistEntry } from '../../models';
+import * as albumAdapter from '../album/album.adapter';
 
-export const adaptArtistFromSpotify = (data: any): NewArtistEntry => {
+export const adaptArtistFromSpotify = (artistFromSpotify: any): NewArtistEntry => {
 	return {
-		id: data.id,
-		name: data.name,
-		avatar: data.images[0]?.url,
-		spotifyUrl: data.external_urls.spotify,
-		uploader: null,
+		id: artistFromSpotify.id,
+		name: artistFromSpotify.name,
+		avatar: artistFromSpotify.images[0]?.url,
+		spotifyUrl: artistFromSpotify.external_urls.spotify,
+		uploader: artistFromSpotify.uploader,
+		albums: albumAdapter.adaptAlbumArrayFromSpotify(artistFromSpotify.albums) as Array<Album>,
 	};
 };
